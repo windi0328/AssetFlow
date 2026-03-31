4416f225b49074d51b1037f971647232cd75feee
Bing Hao <windi0328@yahoo.com.tw>
2026-03-31 17:47:50 +0800

    no message

--- AssetFlow.OMS.Web/obj/Debug/net8.0/AssetFlow.OMS.Web.AssemblyInfo.cs
+++ AssetFlow.OMS.Web/obj/Debug/net8.0/AssetFlow.OMS.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetFlow.OMS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db6101124601f5bbab56d9a53915086217189fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetFlow.OMS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetFlow.OMS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
